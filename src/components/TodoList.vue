63d21a1f9bd08b5da4527f4d6004e5da6f7df578
Marcos <markcosta15@hotmail.com>
2018-08-05 16:27:58 +0200

    New changes

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -97,9 +97,7 @@ export default {
       return this.todos
     },
     showClearCompletedButton () {
-      return this.todos.filter(todo => todo.completed
-      )
-        .length > 0
+      return this.todos.filter(todo => todo.completed).length > 0
     }
   },
   methods: {
